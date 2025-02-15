64d73fd74943f710018ef56ba1c8e15fd4e004ae
Viktor2025-bit <kaluvictor130@gmail.com>
2025-02-15 19:33:34 +0000

    ci pipleline changes

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,7 +1,7 @@
 
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -62,3 +62,11 @@ async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
+
+@router.get("/{book_id}")
+async def get_book_by_id(book_id: int):
+    book = await Book.get_or_none(id=book_id)
+    if not book:
+        raise
+    HTTPException(status_code=404, detail= "Book not found")
+    return book
